e78f664986fc3bf2981db46316d507da0157e254
Vladimir Gordiychuk <folyga@gmail.com>
2016-05-03 21:50:38 +0300

    feat: Add replication settings for Travis CI
    Now user in tests should have replication privilege and also postgres should be configure for accept replication connection, it necessary for test replication protocol

--- travis_install_dependencies.sh
+++ travis_install_dependencies.sh
@@ -4,6 +4,7 @@ set -x -e
 
 sudo service postgresql stop
 sudo cp /etc/postgresql/9.1/main/pg_hba.conf ./
+
 sudo apt-get remove postgresql libpq-dev libpq5 postgresql-client-common postgresql-common -qq --purge
 source /etc/lsb-release
 echo "deb http://apt.postgresql.org/pub/repos/apt/ $DISTRIB_CODENAME-pgdg main ${PG_VERSION}" > pgdg.list
@@ -17,9 +18,25 @@ then
   sudo sed -i -e 's/port = 5433/port = 5432/g' /etc/postgresql/8.4/main/postgresql.conf
 fi
 
-sudo sed -i -e 's/#max_prepared_transactions = 0/max_prepared_transactions = 64/g' /etc/postgresql/${PG_VERSION}/main/postgresql.conf
-
 sudo cp ./pg_hba.conf /etc/postgresql/${PG_VERSION}/main
+
+if [ ${REPLICATION} = 'Y' ]
+then
+    if (( $(echo "${PG_VERSION} >= 9.1" | bc -l)))
+    then
+        sudo sed -i -e 's/#max_wal_senders = 0/max_wal_senders = 4/g' /etc/postgresql/${PG_VERSION}/main/postgresql.conf
+        sudo sed -i -e 's/#wal_keep_segments = 0/wal_keep_segments = 4/g' /etc/postgresql/${PG_VERSION}/main/postgresql.conf
+        sudo sed -i -e 's/^#local\s\+replication\s\+postgres\s\+peer/local replication all peer/g' /etc/postgresql/${PG_VERSION}/main/pg_hba.conf
+        sudo sed -i -e 's/^#host\s\+replication\s\+postgres\s\+\(.*\)\s\+md5/host replication all \1 md5/g' /etc/postgresql/${PG_VERSION}/main/pg_hba.conf
+        if (( $(echo "${PG_VERSION} >= 9.4" | bc -l)))
+        then
+            sudo sed -i -e 's/#wal_level = minimal/wal_level = logical/g' /etc/postgresql/${PG_VERSION}/main/postgresql.conf
+            sudo sed -i -e 's/#max_replication_slots = 0/max_replication_slots = 4/g' /etc/postgresql/${PG_VERSION}/main/postgresql.conf
+        fi
+    fi
+fi
+
+sudo sed -i -e 's/#max_prepared_transactions = 0/max_prepared_transactions = 64/g' /etc/postgresql/${PG_VERSION}/main/postgresql.conf
 
 sudo service postgresql restart ${PG_VERSION}
 
